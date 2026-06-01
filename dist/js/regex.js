0b2a90b3ac16137905c1db5e2208db182e80aa5c
Pedro <eupedrobarbosa03@gmail.com>
2026-05-31 22:00:50 -0300

    dist

--- dist/js/regex.js
+++
@@ -1,7 +0,0 @@
-export const academyRegex = {
-    name: /^(\p{L} ?){5,40}$/giu,
-    cpf: /(^\d{3}\.\d{3}\.\d{3}-\d{2}$)|(^\d{11}$)/gim,
-    telephone: /^\d{11}$/gim,
-    specialty: /^(\p{L} ?){5,40}$/giu,
-    workout: /^(\p{L} ?){5,40}$/giu,
-};
